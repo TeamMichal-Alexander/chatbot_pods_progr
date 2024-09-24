810ae119fa01dfade34c4ae32d9862b30593ad24
BelyaevAlex <belyaevalex2007@gmail.com>
2024-09-24 21:06:58 +0200

    Added prompts.py and rewrite added _generate_prompt_to_sql function

--- src/model.py
+++ src/model.py
@@ -4,11 +4,12 @@ import ollama
 import chromadb
 import pdfplumber
 
-from copy_ollama_function_ChatOllama import ChatOllama
 from langchain_community.utilities import SQLDatabase
 from langchain.chains import create_sql_query_chain
-from connect import ask_ollama_server
+from langchain_experimental.llms.anthropic_functions import prompt
 
+from connect import ask_ollama_server
+from templates.prompts import final_prompt_sql_template, prompt_to_sql_database_template, final_prompt_with_pdf_template
 
 import os
 
@@ -17,14 +18,20 @@ class I:
         pass
 
 class Model:
-    def __init__(self, pdf_path: str):
+    def __init__(self, pdf_path: str, working_with_ollama_server=True):
+        self.logger = logging.getLogger(__name__)
+        self.working_with_ollama_server = working_with_ollama_server
+        logging.basicConfig(filename='py_log.log', encoding='utf-8', level=logging.DEBUG)
         self.pdf_path = pdf_path
         self.model_embedding = "mxbai-embed-large"
         self.model = "llama3.1"
+        if self.working_with_ollama_server:
+            from copy_ollama_function_ChatOllama import ChatOllama
+            self.ollama_generate = ask_ollama_server
+        else:
+            from langchain_community.chat_models import ChatOllama
+            self.ollama_generate = ollama.generate
         self.llm_model_for_sql = ChatOllama(model='llama3.1')
-        self.prompt = ("""Jesteś botem Q&A, ode mnie otrzymasz fragmenty tekstu z planu nauki języka polskiego w szkole ponadpodstawowej, ten plan jest dokładny. Otrzymasz również pytanie, na które musisz odpowiedzieć, opierając się na fragmentach tekstu. Twoja odpowiedź powinna być dokładna, bez zbędnych informacji, ale informatywna i rozszerzona. Ponadto, jeśli to konieczne, możesz zacytować części tekstu w swojej odpowiedzi. Jeśli nie znasz odpowiedzi, poproś o przekształcenie pytania. 
-Oto fragmenty tekstu: [{}]
-Pytanie: [{}]""")
         self.client = chromadb.Client()
         self.collection = self.client.get_or_create_collection(name="docs")
         self.document = self._read_document()
@@ -33,69 +40,7 @@ Pytanie: [{}]""")
         self.db = SQLDatabase.from_uri(f"sqlite:///{self.db_path}")
         self._embedding_document()
         self.chain = create_sql_query_chain(self.llm_model_for_sql, self.db)
-        self.sql_prompt_extractor = lambda x: x[x.find('SELECT'):] + ";" if x[x.find('SELECT'):].count(';') == 0 else x[
-                                                                                                                      x.find(
-                                                                                                                          'SELECT'):x.rfind(
-                                                                                                                          ';') + 1]
-        self.prompt_to_codellama = """Jesteś modelem AI, którego zadaniem jest generowanie zapytań SQL na podstawie mojego opisu. Twoim celem jest wygenerowanie poprawnego zapytania SQL do bazy danych, składającej się z tabel połączonych ze sobą kolumnami z sufiksem id. Te kolumny są używane wyłącznie do łączenia tabel i nie można ich używać do porównywania danych.
-
-        Model nie może używać żadnych danych, które nie zostały bezpośrednio podane w zapytaniu. Model nie może tworzyć ani wymyślać wartości takich jak teacherid, subjectid lub jakichkolwiek innych wartości. Jeśli takie dane są wymagane, ale nie zostały dostarczone w pytaniu, model powinien odpowiedzieć, że nie posiada wystarczających danych do wygenerowania zapytania.
-
-        Jako odpowiedź powinieneś napisać tylko zapytanie do bazy dannych
-
-        Dodatkowo:
-
-        Jeśli w dwóch lub więcej tabelach występują kolumny o tych samych nazwach, należy jasno określić, do której tabeli odnosi się każda kolumna, używając aliasów tabel. To pomoże uniknąć błędów dwuznaczności.
-        Jeśli w zapytaniu użytkownika wspomniany jest dzień tygodnia, model powinien uzyskać id tego dnia z tabeli days.
-        Kiedy połańczasz tablicy między sobą za pomocą lessons.classid zawsze korzystaj się z polecenia lessons.classid LIKE '%' || table_name.column_name || '%'
-        Przykład struktury bazy danych:
-
-        Tabela periods:
-        periodid, period, name, short, starttime, endtime
-        Tabela subjects:
-        subjectid, name, short
-        Tabela teachers:
-        teacherid, firstname, lastname, short, gender, color
-        Tabela classrooms:
-        classroomid, name, short, capacity
-        Tabela groups:
-        groupid, name, classid, entireclass, divisiontag, studentcount
-        Tabela lessons:
-        lessonid, classid, subjectid, teacherid, classroomid, groupid, periodspercard, periodsperweek
-        Tabela days:
-        dayid, name, short, binary
-        Tabela classes:
-        classid, name, short, teacherid, classroomid, grade
-        Instrukcje:
-
-        Używaj JOIN, aby łączyć tabele na podstawie kolumn z sufiksem id.
-        Nie używaj tych kolumn do filtrowania ani porównywania danych.
-        Zawsze używaj LIMIT 1, jeśli użytkownik nie określił liczby wyników.
-        Nigdy nie zapytuj o wszystkie kolumny z tabeli — wybieraj tylko te kolumny, które są potrzebne do odpowiedzi na pytanie.
-        Nazwy kolumn otaczaj w podwójnych cudzysłowach (").
-        Używaj funkcji date('now'), aby uzyskać bieżącą datę, jeśli pytanie dotyczy "dzisiaj".
-        Wykorzystuj tylko te kolumny, które istnieją w bazie danych, i unikaj zapytań o nieistniejące kolumny.
-        Nie wykonuj żadnych dodatkowych porównań na podstawie danych, których nie ma w zapytaniu użytkownika.
-        Nie twórz żadnych wartości identyfikatorów (np. teacherid, subjectid), jeśli nie zostały one jawnie podane w zapytaniu.
-        Jeśli w kilku tabelach istnieją kolumny o takich samych nazwach, zawsze określaj, z której tabeli pochodzi dana kolumna, używając aliasów tabel.
-        Jeśli użytkownik wspomina o dniu tygodnia, model powinien uzyskać odpowiednie id dnia z tabeli days.
-        Jeśli użytkownik nie dostarczył wystarczających danych, odpowiedz, że nie możesz wygenerować zapytania.
-        Jeśli pytanie zawiera odniesienie do bieżącej daty, używaj informacji dostępnych w bazie danych.
-        Jeśli użytkownik nie określa liczby wyników, zwracaj tylko jeden rekord przy użyciu LIMIT.
-        Uporządkuj wyniki, aby zwrócić najbardziej informacyjne dane, jeśli to konieczne.
-        Nigdy nie używaj kolumn z sufiksem id do filtrowania ani porównywania, a jedynie do JOIN.
-        Odpowiadaj ściśle w formacie zapytania SQL, bez komentarzy i zbędnych wyjaśnień.
-        Nazwy dni tygodnia powinni się zaczynać z dużej litery.
-
-        Przykłady: 
-        Pytanie: "Jakiego wychowawcę ma 2cT?" SQLQuery: SELECT T2."firstname", T2."lastname" FROM "classes" AS T1 INNER JOIN "teachers" AS T2 ON T1."teacherid" = T2."teacherid" WHERE T1."name" = '2cT' LIMIT 1; SQLResult: Paweł|Ostrowski Odpowiedź: Wychowawca w klasie 2cT to Paweł Ostrowski.
-        Pytanie: Kto prowadzi lekcji edb? SQLQuery: SELECT T5.firstname, T5.lastname FROM lessons AS T3 INNER JOIN subjects AS T4 ON T3.subjectid = T4.subjectid INNER JOIN teachers AS T5 ON T3.teacherid = T5.teacherid WHERE T4.short_name = 'edb' OR T4.name = 'edb'; SQLResult: Aleksander|Ciwoniuk Odpowiedź: Lekcje edb prowadzi Aleksander Ciwoniuk
-        Pytanie: Wypisz przedmiot który ma 2cT drugą lekcją we wtorek? SQLQuery: SELECT su.name FROM subjects AS su INNER JOIN lessons AS le ON su.subjectid = le.subjectid INNER JOIN classes AS cl ON le.classid LIKE '%' || cl.classid || '%' INNER JOIN cards AS ca ON le.lessonid = ca.lessonid INNER JOIN days AS da ON ca.daysbinary = da.daysbinary WHERE cl.name = '2cT' AND (da.name = 'Wtorek' OR da.name = 'wtorek') AND ca.period = 2; SQLResult: programowanie obiektowe Answer: Drugą lekcją we wtorek 2cT ma programowanie obiektowe
-        Pytanie: Kiedy 2cT ma lekcje testowania i dokumentacji? SQLQuery:  SELECT da.name FROM days as da INNER JOIN cards AS ca ON da.daysbinary = ca.daysbinary INNER JOIN lessons AS le ON ca.lessonid = le.lessonid INNER JOIN classes AS cl ON le.classid LIKE '%' || cl.classid || '%' INNER JOIN subjects AS su ON le.subjectid = su.subjectid WHERE cl.name = '2cT' AND (su.name = 'testowanie i dokumentacja' OR su.short_name = 'testowanie i dokumentacja'); SQLResult: Poniedziałek Answer: 2cT ma lekcję testowania i dokumentacji w poniedziałek
-        Pytanie: Wypisz wszystkie lekcje które ma 2cT w środę? SQLQuery: SELECT su.name FROM subjects AS su INNER JOIN lessons AS le ON su.subjectid = le.subjectid INNER JOIN classes AS cl ON le.classid LIKE '%' || cl.classid || '%' INNER JOIN cards AS ca ON le.lessonid = ca.lessonid INNER JOIN days AS da ON ca.daysbinary = da.daysbinary WHERE cl.name = '2cT' AND (da.name = 'Środa' OR da.name = 'środa'); SQLResult zajęcia z wychowawcą, matematyka, podstawy baz danych, podstawy baz danych, chemia, Przedmiot prcodawcy, Przedmiot prcodawcy, informatyka Answer: 2cT ma zajęcia z wychowawcą, matematyka, podstawy baz danych, podstawy baz danych, chemia, Przedmiot prcodawcy, Przedmiot prcodawcy, informatyka w środę
-
-        Pytanie: {}
-        """
+        self.sql_prompt_extractor = lambda x: x[x.find('SELECT'):] + ";" if x[x.find('SELECT'):].count(';') == 0 else x[x.find('SELECT'):x.rfind(';') + 1]
 
 
     def _read_document(self):
@@ -133,44 +78,43 @@ Pytanie: [{}]""")
             query_embeddings=[response["embedding"]],
             n_results=2)
 
-        print(results['documents'])
+        self.logger.info(results['documents'])
         data = "\n".join([doc[0] for doc in results['documents']])
 
-        output = ask_ollama_server(
+        output = self.ollama_generate(
             model=self.model,
-            prompt=self.prompt.format(data, question)
+            prompt=final_prompt_with_pdf_template.format(data, question)
         )
 
         return output['response']
 
 
-    def ask_sql(self, question):
-        error = 0
-        print(1)
-        response = None
+    def _generate_prompt_to_sql(self, question):
         for i in range(20):
-            response = self.chain.invoke({"question": self.prompt_to_codellama.format(question)})
             try:
-                print(i)
+                response = self.chain.invoke({"question": prompt_to_sql_database_template.format(question)})
                 answer = self.db.run(self.sql_prompt_extractor(response))
-                if len(answer) != 0:
-                    break
+                if len(answer) > 0:
+                    return response
+                else:
+                    continue
             except Exception as e:
-                logging.info(e)
-        else:
-            error = 1
-        if error == 0:
+                self.logger.info(f'{i} trying was unsuccessfully')
+        return None
+
+
+    def ask_sql(self, question):
+        response = self._generate_prompt_to_sql(question)
+        if response:
             db_context = self.sql_prompt_extractor(response)
             db_answer = self.db.run(db_context)
-            print(db_context)
-            print(db_answer)
-            human_qry_template = """Zostaje Ci zadane pytanie: „{question}”.
-        Też masz zapytanie do bazy danych sql: {request_to_sql}.
-        Otrzymujesz także następujące dane z bazy danych, które są zawierają w sobie odpowiedź na pytanie: „{result}”.
-        Korzystając wyłącznie z tych danych, które podałem, krótko i precyzyjnie, ale informatywnie odpowiedz na pytanie. Nie dodawaj nic zbędnego i nie udzielaj wyjaśnień — odpowiadaj tylko na temat. Przy przeliczeniu w celu rozdzielenia wykorzystuj przycinek. 
-        """
-            print(human_qry_template.format(question=question, result=db_answer, request_to_sql=db_context))
-            response = ask_ollama_server(prompt=human_qry_template.format(question=question, result=db_answer, request_to_sql=db_context), model=self.model)
+            self.logger.info(db_context)
+            self.logger.info(db_answer)
+            self.logger.info(final_prompt_sql_template.format(question=question, result=db_answer, request_to_sql=db_context))
+            if self.working_with_ollama_server:
+                response = self.ollama_generate(prompt=final_prompt_sql_template.format(question=question, result=db_answer, request_to_sql=db_context), model=self.model)
+            else:
+                response = ollama.generate(prompt=final_prompt_sql_template.format(question=question, result=db_answer, request_to_sql=db_context), model=self.model)
             return response['response']
         else:
             return 'Niestety nie udało się znaleść jakiejkolwiek informacji, sprobójcie przeformulować prompt lub zapytajcie o czymś innym'
@@ -184,4 +128,3 @@ Pytanie: [{}]""")
             answer = self.ask_sql(question)
         json_answer = {'answer': answer}
         return json_answer
-
